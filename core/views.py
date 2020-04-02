4464f8c76dca2a8d48ce43e98da46a47f0479f62
JHEmon <jahid.h.emon@gmail.com>
2020-04-03 02:10:23 +0600

    initial commit

--- core/views.py
+++ core/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from .models import Item
 # Create your views here.
+
+def item_list(requset):
+    context = {
+        'items': Item.objects.all
+    }
+    return render(requset, "home.html", context)
+
+def CheckoutView(request):
+    return render(request, "checkout.html")
+
+def ProductsView(request):
+    return render(request, "products.html")
--- djecommerce/urls.py
+++ djecommerce/urls.py
@@ -21,10 +21,12 @@ from .views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),
+    path('accounts/', include('allauth.urls')),
+    path('', include('core.urls', namespace='core')),
 ]
 
-#Show debug toolbar
+# Show debug toolbar
 if settings.DEBUG:
     import debug_toolbar
+
     urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
